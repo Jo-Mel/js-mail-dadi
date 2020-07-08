6ba4b2cd84e20dfb056d6861562217f311b9a9d2
Jona M <jonameleqi@gmail.com>
2020-07-08 19:16:09 +0200

    ulteriori migliorie

--- js/script.js
+++ js/script.js
@@ -7,14 +7,16 @@ var mailAdd = prompt('Inserisci qui la tua mail');
 var mailingList = ['prova@gmail.com', 'example@gmail.com', 'nome.cognome@gmail.it', 'example-insert@gmail.com'];
 var trovata = false;
 
+
 for (var i = 0; i < mailingList.length; i++) {
     if (mailAdd == mailingList[i]) {
         trovata = true;
+        break;
     }
 }
 
 if (trovata == true) {
     alert ('Mail riconosciuta. Vai avanti');
-}else {
+} else {
     alert ('Mail non trovata');
 }
